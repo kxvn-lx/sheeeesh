67c007b64c93ce5fd2c9ab88252f967077582e33
Kevin Laminto <kevin.laminto@gmail.com>
2021-04-05 16:23:20 +1000

    Initial Commit

---
+++ Sheeeesh/SheeeeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheeeeshApp.swift
+//  Sheeeesh
+//
+//  Created by Kevin Laminto on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SheeeeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
